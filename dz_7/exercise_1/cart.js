32fd106ffe91dc7ff12814ff3cabd8fde21f1f1d
Michael <Cholinesterasa@gmail.com>
2022-04-22 15:15:23 +0300

    dz_7

---
+++ dz_7/exercise_1/cart.js
@@ -0,0 +1,133 @@
+let cart = [];
+const mainElement = document.querySelector('main');
+const cartInfoElement = document.querySelector('#cart')
+
+//получение данных из  странницы каталога
+function loadCart() {
+    cart = JSON.parse(window.localStorage.getItem('userCart')) || [];
+}
+
+loadCart();
+
+
+console.log(cart)
+//создание карточек в корзине
+function drawProductCard(cart) {
+    const cardElement = document.createElement('DIV');
+    const imgElement = document.createElement('IMG');
+    const titleElement = document.createElement('P');
+    const priceElement = document.createElement('p');
+    const quantityElement = document.createElement('p');
+    const addElement = document.createElement('BUTTON');
+    const removeElement = document.createElement('BUTTON');
+
+    cardElement.classList.add('product-card');
+    priceElement.classList.add('product-card__price')
+    titleElement.classList.add('product-card__title');
+    imgElement.classList.add('product-card__img');
+    addElement.classList.add('product-card__button');
+    addElement.classList.add('plus');
+    removeElement.classList.add('product-card__button');
+    removeElement.classList.add('minus');
+    cardElement.classList.add(`${cart.id}`)
+
+    imgElement.setAttribute('src', cart.img);
+    titleElement.textContent = cart.name;
+    priceElement.textContent = cart.price + 'p';
+    addElement.textContent = 'Добавить';
+    addElement.setAttribute('data-id', cart.id)
+    removeElement.textContent = 'Удалить';
+    removeElement.setAttribute('data-id', cart.id);
+
+
+    cardElement.append(imgElement);
+    cardElement.append(titleElement);
+    cardElement.append(priceElement);
+
+    cardElement.append(addElement);
+    cardElement.append(removeElement);
+
+    mainElement.append(cardElement);
+
+}
+
+function drawProducts() {
+    for (let product of cart) {
+        drawProductCard(product);
+    }
+}
+// функция расчета цены в корзине
+function getCartPrice() {
+    let result = 0;
+    for (let product of cart) {
+        result += product.price * product.quantity;
+    }
+    return result;
+}
+// функция расчета количества товаров в корзине
+
+function getCartQuantity() {
+    let result = 0;
+    for (let product of cart) {
+        result += product.quantity;
+    }
+    return result;
+}
+
+// получение информации о составе корзины
+function getCartInfo() {
+    if (cart.length == 0) {
+        return 'Корзина пуста';
+    }
+    return `в корзине ${getCartQuantity()} товаров на сумму ${getCartPrice()} р`;
+}
+
+// функция вывода состава корзины
+function drawCartInfo() {
+    cartInfoElement.textContent = getCartInfo();
+}
+
+drawProducts();
+drawCartInfo();
+
+//функция удаления товара из корзины
+function addToCart(id) {
+    const existProduct = cart.find(function (product) { return product.id == id });
+    existProduct.quantity++;
+    drawCartInfo();
+    return console.log('добавлено в корзину');
+}
+
+
+//функия добавления товара в корзину
+function removefromCart(id) {
+    const existProduct = cart.find(function (product) { return product.id == id });
+    if (existProduct.quantity >= 1) {
+        existProduct.quantity--;
+        drawCartInfo();
+    }
+    if (existProduct.quantity == 0) {
+        drawCartInfo();
+        return -1;
+
+    }
+
+}
+// добавление токара в корзину
+mainElement.addEventListener('click', function (event) {
+    if (event.target.classList.contains('plus')) {
+        addToCart(event.target.dataset.id);
+    }
+})
+
+//удаление товара из корзины
+mainElement.addEventListener('click', function (event) {
+    let a;
+    if (event.target.classList.contains('minus')) {
+        a = removefromCart(event.target.dataset.id);
+    }
+    if (a == -1) {
+        console.log('ok')
+        event.target.closest('.product-card').remove();
+    }
+})
---
+++ dz_7/exercise_2/main.js
@@ -0,0 +1,110 @@
+const products = [
+    { name: 'rubber', price: 8000, img: 'img/rubber.jfif', quantity: 1, id: 1 },
+    { name: 'blade', price: 15000, img: 'img/blade.jfif', quantity: 1, id: 2 },
+    { name: 'glue', price: 400, img: 'img/glue.jfif', quantity: 1, id: 3 }
+];
+let cart = []
+
+const mainElement = document.querySelector('main');
+const cartInfoElement = document.querySelector('#cart')
+
+
+
+function loadProducts() {
+    for (let i = 0; i < 3; i++) {
+        cart.push(products[i]);
+    }
+}
+
+function drawProductCard(products) {
+    const cardElement = document.createElement('DIV');
+    const imgElement = document.createElement('IMG');
+    const titleElement = document.createElement('P');
+    const priceElement = document.createElement('p');
+    const btnElement = document.createElement('BUTTON');
+
+    cardElement.classList.add('product-card');
+    priceElement.classList.add('product-card__price')
+    titleElement.classList.add('product-card__title');
+    imgElement.classList.add('product-card__img');
+    btnElement.classList.add('product-card__button');
+
+    imgElement.setAttribute('src', products.img);
+    titleElement.textContent = products.name;
+    priceElement.textContent = products.price + 'p';
+    btnElement.textContent = 'в корзину';
+    btnElement.setAttribute('data-id', products.id)
+
+    cardElement.append(imgElement);
+    cardElement.append(titleElement);
+    cardElement.append(priceElement);
+    cardElement.append(btnElement);
+
+    mainElement.append(cardElement);
+
+}
+
+function drawProducts() {
+    for (let product of products) {
+        drawProductCard(product);
+    }
+}
+
+function getCartPrice() {
+    let result = 0;
+    for (let product of cart) {
+        result += product.price * product.quantity;
+    }
+    return result;
+}
+
+function getCartQuantity() {
+    let result = 0;
+    for (let product of cart) {
+        result += product.quantity;
+    }
+    return result;
+}
+
+function getCartInfo() {
+    if (cart.length == 0) {
+        return 'Корзина пуста';
+    }
+    return `в корзине ${getCartQuantity()} товаров на сумму ${getCartPrice()} р`;
+}
+
+function drawCartInfo() {
+    cartInfoElement.textContent = getCartInfo();
+}
+
+function addToCart(id) {
+    const existProduct = cart.find(function (product) { return product.id == id });
+    if (existProduct) {
+        existProduct.quantity++;
+        drawCartInfo();
+        saveCart();
+        return;
+    }
+    const product = Object.assign({}, products.find(function (product) { return product.id == id }));
+    cart.push(product);
+    saveCart();
+    drawCartInfo();
+
+}
+
+function saveCart() {
+    window.localStorage.setItem('userCart', JSON.stringify(cart));
+}
+
+function loadCart() {
+    cart = JSON.parse(window.localStorage.getItem('userCart')) || [];
+}
+drawProducts();
+drawCartInfo();
+// loadCart();
+
+mainElement.addEventListener('click', function (event) {
+    if (event.target.tagName == 'BUTTON') {
+        addToCart(event.target.dataset.id)
+    }
+})
